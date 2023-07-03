d74ffe3df6ca7b396755b237aa2f11b42e65476f
Jason Williscroft <jason.williscroft@veterancrowd.com>
2023-07-03 16:31:11 +0800

    [WEB-1041] added parseEnvFromBranch

--- test/setup.js
+++ test/setup.js
@@ -1,13 +1,13 @@
 /* eslint-env mocha */
 
-import { getDotenv } from '@karmaniverous/get-dotenv';
+// lib imports
+import { testSetup } from '../lib/testSetup.js';
+
+// Import version from package.json.
+import pkg from '../package.json' assert { type: 'json' };
+if (!process.env.npm_package_version)
+  process.env.npm_package_version = pkg.version;
 
 before(async function () {
-  if (!process.env.ENV)
-    await getDotenv({
-      dynamicPath: './env/dynamic.js',
-      env: 'dev',
-      loadProcess: true,
-      paths: ['./', './env'],
-    });
+  await testSetup();
 });
