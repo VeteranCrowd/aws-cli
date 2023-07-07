185ffbe6b4cb050788a77939150cd98280a31441
Jason Williscroft <jason.williscroft@veterancrowd.com>
2023-07-07 09:46:49 +0800

    [WEB-1041] added aws-authed shell commands

--- bin/vc/index.js
+++ bin/vc/index.js
@@ -2,8 +2,9 @@
 
 // npm imports
 import { Logger } from '@karmaniverous/edge-logger';
-import { getDotenvCli } from '@karmaniverous/get-dotenv';
+import { dotenvExpand, getDotenvCli } from '@karmaniverous/get-dotenv';
 import { Command } from 'commander';
+import { execaCommand } from 'execa';
 import fs from 'fs-extra';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
@@ -28,10 +29,40 @@ const aws = new Command()
   .configureHelp({ showGlobalOptions: true })
   .enablePositionalOptions()
   .passThroughOptions()
-  .hook('preSubcommand', async () => {
+  .option(
+    '-c, --command <string>',
+    'dotenv-expanded shell command string',
+    dotenvExpand
+  )
+  .hook('preSubcommand', async (thisCommand) => {
     if (process.env.AWS_LOCAL_PROFILE)
       await getAwsSsoCredentials(process.env.AWS_LOCAL_PROFILE);
+
+    // Execute shell command.
+    const { command } = thisCommand.opts();
+    if (command)
+      await execaCommand(command.replace(/ /g, '\\ '), {
+        shell: true,
+        stdio: 'inherit',
+      });
   })
+  .addCommand(
+    new Command()
+      .name('cmd')
+      .description('execute shell command string (default command)')
+      .configureHelp({ showGlobalOptions: true })
+      .enablePositionalOptions()
+      .passThroughOptions()
+      .action(async (options, command) => {
+        const { args } = command;
+        if (args.length)
+          await execaCommand(args.join('\\ '), {
+            stdio: 'inherit',
+            shell: true,
+          });
+      }),
+    { isDefault: true }
+  )
   .addCommand(pullSecret)
   .addCommand(pushSecret)
   .addCommand(deleteSecret)
