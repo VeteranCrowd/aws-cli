4a327482d38d00a986d6402c93e8773360e6048a
Jason Williscroft <jason.williscroft@veterancrowd.com>
2024-01-11 18:00:54 +0800

    [WEB-1381] added fix-autoscale command

--- env/dynamic.js
+++ env/dynamic.js
@@ -1,6 +1,6 @@
 () => ({
-  API_VERSION: () =>
-    `v${(process.env.npm_package_version ?? '').split('.')[0]}`,
+  API_VERSION: () => `v0`,
+  // `v${(process.env.npm_package_version ?? '').split('.')[0]}`,
   BRANCH_NAME: ({ ENV, PROD_ENV_TOKEN }) =>
     ENV === PROD_ENV_TOKEN ? 'main' : ENV,
   SERVICE_NAME: ({ API_CONTEXT, API_SUBDOMAIN }) =>
