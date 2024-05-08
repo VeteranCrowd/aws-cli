7dbdd2b0d81242cfe65a087dd94d7f20f44a33c2
Jason Williscroft <jason.williscroft@veterancrowd.com>
2024-05-08 18:47:36 +0800

    [WEB-1584] updated dependencies, switched to get-dotenv 4

--- bin/vc/index.js
+++ bin/vc/index.js
@@ -1,35 +1,16 @@
 #!/usr/bin/env node
 
 // npm imports
-import { Logger } from '@karmaniverous/edge-logger';
-import { getDotenvCli } from '@karmaniverous/get-dotenv';
-import fs from 'fs-extra';
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { generateGetDotenvCli } from '@karmaniverous/get-dotenv';
 
 // lib imports
 import { aws } from '../../lib/aws/index.js';
-import { bulk } from '../../lib/bulk/index.js';
 import { dc } from '../../lib/depcheck.js';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // lib imports
 import { getDefaultEnv } from '../../lib/getDefaultEnv.js';
 
-// Load default options.
-const cliDefaultOptionsCustomPath = resolve(
-  __dirname,
-  '../../getdotenv.config.json'
-);
-
-const cliDefaultOptionsCustom = (await fs.exists(cliDefaultOptionsCustomPath))
-  ? JSON.parse(await fs.readFile(cliDefaultOptionsCustomPath))
-  : {};
-
-const cli = getDotenvCli({
-  ...cliDefaultOptionsCustom,
-  logger: new Logger(),
+const cli = generateGetDotenvCli({
   preHook: async (options) => {
     const defaultEnv = await getDefaultEnv();
     if (defaultEnv) options.defaultEnv = defaultEnv;
@@ -37,7 +18,6 @@ const cli = getDotenvCli({
   },
 })
   .addCommand(aws)
-  .addCommand(bulk)
   .addCommand(dc);
 
 await cli.parseAsync();
