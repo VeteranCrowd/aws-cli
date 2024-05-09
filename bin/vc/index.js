1b88732ef9f6f44b0d33b5bcbb08bdf51f825fe8
Jason Williscroft <jason.williscroft@veterancrowd.com>
2024-05-09 17:28:30 +0800

    updated dependencies

--- bin/vc/index.js
+++ bin/vc/index.js
@@ -2,36 +2,22 @@
 
 // npm imports
 import { generateGetDotenvCli } from '@karmaniverous/get-dotenv';
-import fs from 'fs-extra';
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
 
 // lib imports
 import { aws } from '../../lib/aws/index.js';
 import { dc } from '../../lib/depcheck.js';
 import { getDefaultEnv } from '../../lib/getDefaultEnv.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const defaultGetDotenvCliOptionsPath = resolve(
-  __dirname,
-  '../getdotenv.config.json'
-);
-
-const defaultGetDotenvCliOptions = (await fs.exists(
-  defaultGetDotenvCliOptionsPath
-))
-  ? JSON.parse(await fs.readFile(defaultGetDotenvCliOptionsPath).toString())
-  : {};
-
-const cli = generateGetDotenvCli({
-  ...defaultGetDotenvCliOptions,
-  preHook: async (options) => {
-    const defaultEnv = await getDefaultEnv();
-    if (defaultEnv) options.defaultEnv = defaultEnv;
-    return options;
-  },
-})
+const cli = (
+  await generateGetDotenvCli({
+    importMetaUrl: import.meta.url,
+    preHook: async (options) => {
+      const defaultEnv = await getDefaultEnv();
+      if (defaultEnv) options.defaultEnv = defaultEnv;
+      return options;
+    },
+  })
+)
   .addCommand(aws)
   .addCommand(dc);
 
